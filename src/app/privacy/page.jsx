3506e27185e00c0a0679edd7f949ff346bbc3cbd
jazzgibran <jazz.gibran@gmail.com>
2024-05-12 16:09:30 +0800

    little updates

--- src/app/privacy/page.jsx
+++ src/app/privacy/page.jsx
@@ -3,7 +3,7 @@ const PrivacyPolicy = () => {
     return (
         <div className="min-h-screen flex flex-col justify-center items-center">
 
-            <div className="px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto">
+            <div className="px-4 py-12 sm:px-6 lg:px-8 max-w-7xl mx-auto">
                 <div className="max-w-3xl mx-auto bg-Putty bg-opacity-50 p-8 shadow-lg rounded-lg">
                     <h1 className="text-3xl font-bold text-gray-800 mb-6">Privacy Policy</h1>
                     <p className="text-gray-700 mb-6">
